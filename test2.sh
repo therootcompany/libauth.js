f36ed5db1196fe5fa277491bc848f1e702985d87
AJ ONeal <coolaj86@gmail.com>
2021-06-17 15:26:44 -0600

    WIP: magic link: complete order/verify/fulfill cycle

--- test2.sh
+++ test2.sh
@@ -1,14 +1,30 @@
+#!/bin/bash
+set -e
+set -u
+
 source .env
 
 function order() {
-    curl -X POST http://localhost:${PORT}/api/authn/challenge/issue \
-        -H 'Content-Type: application/json' \
-        -d '{ "type": "email", "value": "coolaj86@gmail.com" }'
+    local my_challenge_token="$(
+        curl -X POST http://localhost:${PORT}/api/authn/challenge/issue \
+            -H 'Content-Type: application/json' \
+            -d '{ "type": "email", "value": "coolaj86@gmail.com" }' |
+            jq -r '.challenge_token'
+    )"
+    echo "challenge_token:"
+    echo "${my_challenge_token}"
+    echo ''
+
+    echo ''
+    curl "http://localhost:${PORT}/api/authn/challenge?challenge_token=${my_challenge_token}"
+    echo ''
     echo ''
 }
 
 function finalize() {
-    my_secret="${1}"
+    local my_secret="${1}"
+    local my_challenge_token="${2}"
+
     echo ''
     curl "http://localhost:${PORT}/api/authn/challenge?token=${my_secret}"
     echo ''
@@ -19,7 +35,20 @@ function finalize() {
         -d '{ "token": "'${my_secret}'" }'
     echo ''
     echo ''
+
+    echo ''
+    curl "http://localhost:${PORT}/api/authn/challenge?challenge_token=${my_challenge_token}"
+    echo ''
+    echo ''
+
+    echo ''
+    curl -X POST http://localhost:${PORT}/api/authn/challenge/exchange \
+        -H 'Content-Type: application/json' \
+        -d '{ "challenge_token": "'${my_challenge_token}'" }'
+    echo ''
+    echo ''
 }
 
 #order
-finalize R70gjeDLltGo4iKOjL52Vg
+#finalize XXXXXXXXXXXXXXXXXXXXXX xxxx.yyyy.zzzz
+finalize v42JOYrMlkFyBGcQmpbukg eyJ0eXAiOiJKV1QiLCJraWQiOiJhTk1VcFBxazFUaVdKNXZXeG5nWHRHbWhob0JNTmFGOFpmZkZSa1QyWVBrIiwiYWxnIjoiRVMyNTYifQ.eyJjaGFsbGVuZ2VfaWQiOiJrclp1YmQzQXBYK1VkRU5kTzlmZDFSIiwidHlwZSI6ImVtYWlsIiwidmFsdWUiOiJjb29sYWo4NkBnbWFpbC5jb20iLCJpYXQiOjE2MjM5NjQ5MjYsImV4cCI6MTYyMzk2ODUyNiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDozMDQyIn0.JOtdACMFvKFvIgbUMV1gRXzqHAHyC-MTOCr9VaiAjJeEKyEd9BF4TYuyIR3FplY9NKXFhVv1z1ZY5TfpRe19ZA
