0cacb040514690e1a4b494e7c57271744ee6459a
AJ ONeal <aj@therootcompany.com>
2021-09-09 06:12:24 +0000

    refactor: move common Auth3000 functions to own file

--- public/js/main.js
+++ public/js/main.js
@@ -26,96 +26,6 @@
     throw err;
   }
 
-  function querystringify(options) {
-    return Object.keys(options)
-      .filter(function (key) {
-        return (
-          "undefined" !== typeof options[key] &&
-          null !== options[key] &&
-          "" !== options[key]
-        );
-      })
-      .map(function (key) {
-        // the values must be URI-encoded (the %20s and such)
-        return key + "=" + encodeURIComponent(options[key]);
-      })
-      .join("&");
-  }
-
-  function generateOidcUrl(
-    oidcBaseUrl,
-    client_id,
-    redirect_uri,
-    scope,
-    login_hint
-  ) {
-    // a secure-enough random state value
-    // (all modern browsers use crypto random Math.random, not that it much matters for a client-side state cache)
-    var rnd = Math.random().toString();
-    // transform from 0.1234... to hexidecimal
-    var state = parseInt(rnd.slice(2).padEnd(16, "0"), 10)
-      .toString(16)
-      .padStart(14, "0");
-    // response_type=id_token requires a nonce (one-time use random value)
-    // response_type=token (access token) does not
-    var nonceRnd = Math.random().toString();
-    var nonce = parseInt(nonceRnd.slice(2).padEnd(16, "0"), 10)
-      .toString(16)
-      .padStart(14, "0");
-    var options = { state, client_id, redirect_uri, scope, login_hint, nonce };
-    // transform from object to 'param1=escaped1&param2=escaped2...'
-    var params = querystringify(options);
-    return oidcBaseUrl + "?response_type=id_token&access_type=online&" + params;
-  }
-
-  function generateOauth2Url(
-    authorize_url,
-    client_id,
-    redirect_uri,
-    scopes,
-    login_hint
-  ) {
-    // <!-- redirect_uri=encodeURIComponent("https://beyondcode.duckdns.org/api/webhooks/oauth2/github") -->
-    // <!-- scope=encodeURIComponent("read:user%20user:email") -->
-    // <a href="https://github.com/login/oauth/authorize?client_id=0b7cdfa2fde2f019a3b5&redirect_uri=https%3A%2F%2Fbeyondcode.duckdns.org%2Fapi%2Fwebhooks%2Foauth2%2Fgithub&scope=read:user%20user:email&state=1234567890&allow_signup=true&login=">
-
-    // a secure-enough random state value
-    // (all modern browsers use crypto random Math.random, not that it much matters for a client-side state cache)
-    var rnd = Math.random().toString();
-    // transform from 0.1234... to hexidecimal
-    var state = parseInt(rnd.slice(2).padEnd(16, "0"), 10)
-      .toString(16)
-      .padStart(14, "0");
-    var login = login_hint;
-    var scope = scopes.join(" ");
-    var options = {
-      state,
-      client_id,
-      redirect_uri,
-      scope,
-      login: login_hint,
-    };
-    var params = querystringify(options);
-    return authorize_url + "?allow_signup=true&" + params;
-  }
-
-  // because JavaScript's Base64 implementation isn't URL-safe
-  function urlBase64ToBase64(str) {
-    var r = str % 4;
-    if (2 === r) {
-      str += "==";
-    } else if (3 === r) {
-      str += "=";
-    }
-    return str.replace(/-/g, "+").replace(/_/g, "/");
-  }
-
-  function urlBase64ToJson(u64) {
-    var b64 = urlBase64ToBase64(u64);
-    var str = atob(b64);
-    return JSON.parse(str);
-  }
-
   async function attemptRefresh() {
     let resp = await window
       .fetch(baseUrl + "/api/authn/refresh", { method: "POST" })
@@ -149,36 +59,12 @@
     console.info(dummies);
   }
 
-  function parseQuerystring(querystring) {
-    var query = {};
-    querystring.split("&").forEach(function (pairstring) {
-      var pair = pairstring.split("=");
-      var key = pair[0];
-      var value = decodeURIComponent(pair[1]);
-
-      query[key] = value;
-    });
-    return query;
-  }
-
-  async function parseJwt(jwt) {
-    var parts = jwt.split(".");
-    var jws = {
-      protected: parts[0],
-      payload: parts[1],
-      signature: parts[2],
-    };
-    jws.header = urlBase64ToJson(jws.protected);
-    jws.claims = urlBase64ToJson(jws.payload);
-    return jws;
-  }
-
   async function init() {
     $(".js-logout").hidden = true;
     $(".js-social-login").hidden = true;
 
     // TODO rename to google
-    var googleSignInUrl = generateOidcUrl(
+    var googleSignInUrl = Auth3000.generateOidcUrl(
       "https://accounts.google.com/o/oauth2/v2/auth",
       ENV.GOOGLE_CLIENT_ID,
       ENV.GOOGLE_REDIRECT_URI,
@@ -187,7 +73,7 @@
     );
     $(".js-google-oidc-url").href = googleSignInUrl;
 
-    var githubSignInUrl = generateOauth2Url(
+    var githubSignInUrl = Auth3000.generateOauth2Url(
       "https://github.com/login/oauth/authorize",
       ENV.GITHUB_CLIENT_ID,
       ENV.GITHUB_REDIRECT_URI,
@@ -210,7 +96,7 @@
     });
 
     var querystring = document.location.hash.slice(1);
-    var query = parseQuerystring(querystring);
+    var query = Auth3000.parseQuerystring(querystring);
     if (query.id_token) {
       completeOidcSignIn(query);
       return;
@@ -281,7 +167,7 @@
     // Show the token for easy capture
     console.info("id_token", query.id_token);
 
-    let jws = await parseJwt(query.id_token).catch(die);
+    let jws = await Auth3000.parseJwt(query.id_token).catch(die);
 
     if ("https://accounts.google.com" === jws.claims.iss) {
       // TODO make sure we've got the right options for fetch !!!
